5cdae3a5f3ee3d9201368095e4341eae1880fd02
brkayucar <berkay70ucar@gmail.com>
2026-01-11 15:38:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function git(){
+let user = document.getElementById("username").value;
+
+if(user==""){
+alert("Kullanıcı adı gir!");
+return;
+}
+
+window.location.href="sonuc.html?u="+user;
+}
+
 
